722481b881dbfc7bcbb9dc0aa53fa2cf0a2a5aea
Lucas Cardoso dos Santos <lucas.cardoso@dce.ufpb.br>
2021-05-16 20:24:09 -0300

    All system classes created, remaning declaration

---
+++ src/sys/AlunoNaoCadastradoException.java
@@ -0,0 +1,30 @@
+package sys;
+
+public class AlunoNaoCadastradoException extends Exception {
+
+	public AlunoNaoCadastradoException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlunoNaoCadastradoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlunoNaoCadastradoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlunoNaoCadastradoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AlunoNaoCadastradoException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/sys/SysPlanilha.java
+++ src/sys/SysPlanilha.java
@@ -1,9 +1,45 @@
 package sys;
 
+import java.util.LinkedList;
+
 public class SysPlanilha {
 
+	private LinkedList<Aluno> alunos = new LinkedList<Aluno>();
+	
 	public SysPlanilha() {
-		// TODO Auto-generated constructor stub
+		this.carregarDadosBasicos();
+	}
+	
+	public void cadastrarAluno(Aluno aluno) throws AlunoJaCadastradoException{
+		
+	}
+	
+	public Aluno pesquisarAlunoPorMatricula(String matricula) throws AlunoNaoCadastradoException{
+		return null;
+	}
+	
+	public Aluno pesquisarAlunoPorNome(String matricula) throws AlunoNaoCadastradoException{
+		return null;
+	}
+	
+	public void editarNotaDoAluno(String matricula, NomesMateria materia, double nota) throws AlunoNaoCadastradoException{
+		
+	}
+	
+	public void excluirAluno(String matricula) throws AlunoNaoCadastradoException{
+		
+	}
+	
+	public void carregarDadosBasicos() {
+		//TODO: implementar o load dos dados de matriculas
+	}
+	
+	public void carregarAluno(String matricula)  throws AlunoNaoCadastradoException{
+		//TODO: imlementar o lazyload de alunos
+	}
+	
+	public void salvarDados() {
+		//TODO: implementar o save de todos os dados em cache
 	}
 
 }
